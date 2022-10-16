a6a7e58c9530aad0d034198ac6b812f514a42e6f
marcos-mi <marcosmino.dev@gmail.com>
2022-10-16 02:48:34 -0300

    promesas en cadena

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 
 const chistesUrl = "https://api.chucknorris.io/jokes/random";
-fetch( chistesUrl ).then(resp =>{
-    resp.json().then( data =>{
-        console.log(data.id)
-        console.log(data.value)
-    });
-});
+// fetch( chistesUrl ).then(resp =>{
+//     resp.json().then( data =>{
+//         console.log(data.id)
+//         console.log(data.value)
+//     });
+// });
+
+fetch(chistesUrl)
+        .then(resp => resp.json())
+        .then ( ({id,value}) => {
+            console.log(id, value);
+        });
